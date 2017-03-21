b4be2d3f3add019bafc728360a7caf8f65a38d39
yh1008 <ye.hua@columbia.edu>
2017-03-21 17:04:18 -0400

    add key from try4-9

--- Backend/TwitterRecorder/src/config.ts
+++ Backend/TwitterRecorder/src/config.ts
@@ -21,4 +21,28 @@ export const watsonConfig = [{
     username: 'fb0d03af-cea5-438c-8c95-7857111a021a',
     password: 'sUMEuRhRPeoK',
     version_date: '2017-02-27'
+}, {
+   username: "c92945a8-daec-416e-8eae-8bfc869d48ea",
+   password: "cFP3uFBrHD0N",
+   version_date: '2017-02-27'
+}, {
+   username: "2e96864b-156a-43d7-8059-2faecffb5c3c",
+   password: "ESPDZA7jpv6j",
+   version_date: '2017-02-27'
+}, {
+   username: "56279904-a714-4bc8-b507-9168a79420c6",
+   password: "PRUF0nHH4qFF",
+   version_date: '2017-02-27'
+}, {
+   username: "7a3c6df7-6a51-4b97-a790-f283c356426f",
+   password: "ghjNtTnAOYyu",
+   version_date: '2017-02-27'
+}, {
+   username: "0d0d1223-938f-48c0-aad4-09965a813c43",
+   password: "zopsgXfhTpQp",
+   version_date: '2017-02-27'
+}, {
+   username: "d59e1190-5913-435b-967d-2e2dc79b9100",
+   password: "DIT6MbBKVVjO",
+   version_date: '2017-02-27'
 }];
